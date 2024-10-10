1874d38c0782d55e366e605a0be7441132316a78
Okami <netbratella@gmail.com>
2024-10-10 14:57:10 +0300

    Задачка на scope

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
 fn main() {
     let name = "дружбан".to_string();
     println!("Привет, мой {name} (^_^)/\n Вот и пример релиза.");
+
+    let mut x = 1;
+    {
+        let mut x = x;
+        x += 2;
+    }
+    println!("{x}");
 }
